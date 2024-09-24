a2c553eb3612face66c0775fa5f9f8f21e9dcc21
Choiji92 <jeahoon100@naver.com>
2024-09-24 20:14:08 +0900

    [FEAT] font 추가 및 data fetch 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
+import { useEffect, useState } from 'react'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className={cx('container')}>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+  // 1. wedding 데이터 호출
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.log(e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
